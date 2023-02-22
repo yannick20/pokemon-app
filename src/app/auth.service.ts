8a2eb64f75573a094f31c31c65ea7284400ece77
Potter <37401491+yannick20@users.noreply.github.com>
2023-02-22 01:37:12 +0100

    updated 01

---
+++ src/app/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { delay, Observable, of, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLoggedIn: boolean = false;
+  redirectUrl: string | undefined;
+
+  login(name: string, password: string): Observable<boolean> {
+    const isLoggedIn = (name == 'admin' && password == 'admin');
+
+    return of(isLoggedIn).pipe(
+      delay(1000),
+      tap(isLoggedIn => this.isLoggedIn = isLoggedIn)
+    );
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+  }
+}
